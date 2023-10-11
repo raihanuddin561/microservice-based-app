a74342c5a992a98c0143c642954f2b00778f406a
raihanuddin561 <raihanuddin561@gmail.com>
2023-10-11 22:07:01 +0600

    User Controller and Global Exception handler added

---
+++ mobile-app-ws/src/main/java/com/developersblog/app/ws/constants/AppConstants.java
@@ -0,0 +1,9 @@
+package com.developersblog.app.ws.constants;
+
+public class AppConstants {
+    /**
+     * api end points
+     */
+    public static final String USER_ENDPOINT = "/api/user";
+
+}
