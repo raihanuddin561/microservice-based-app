1f8ecde757271aa3e05c6cffc029b32b97443a0c
raihanuddin561 <raihanuddin561@gmail.com>
2023-11-01 21:02:20 +0600

    save user info in db functionalities added

---
+++ userService/src/main/java/com/developerblog/userService/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.developerblog.userService.repository;
+
+import com.developerblog.userService.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+}
